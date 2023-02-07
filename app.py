95e78ccee654639ba64b2ec7b549c5d5406b298e
merderinmike <69906934+merderinmike@users.noreply.github.com>
2023-02-07 15:02:33 -0600

    .

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ cluster = MongoClient(
 db = cluster["materialTracker"]
 projects = db['Projects']
 materials = db['Materials']
-# ---Create tabs---
+
+# ---Create tabs, columns---
 tab1, tab2 = st.tabs(['Add Project', 'Add Project Material'])
 
 # ---Add project----
@@ -25,10 +26,39 @@ with tab1:
         projectName = st.text_input("Enter the project name")
         projectLocation = st.text_input("Please enter the facility for the project")
         projectStart = st.date_input("What date did the project start?")
-        projectEnd = st.date_input("What date did the project end?")
-        projectLength = projectStart - projectEnd
+        #projectEnd = st.date_input("What date did the project end?")
+        #projectLength = projectEnd - projectStart
+        if st.form_submit_button("Submit"):
+            if projects.count_documents({'_id': projectName}) != 0:
+                st.error("Project already exists!")
+            else:
+                db['Projects'].insert_one({"_id":projectName, "Location":projectLocation,"Start":projectStart})
+                st.success("Project created successfully!")
+                time.sleep(2)
+                st.experimental_rerun()
 
-        submit = st.form_submit_button("Submit")
-        if submit:
-            time.sleep(2)
-            st.experimental_rerun()
+with tab2:
+    col1, col2 = st.columns(2)
+    with col1:
+        st.subheader("Add material to a project")
+        #projectSelect = list(projects.find())
+        #findMaterials = list(materials.find())
+        selection = ["Select A Project"]
+        material = []
+        #for project in projectSelect:
+            #selection.append(f"{project['_id']}")
+        #for item in findMaterials:
+            #material.append(f"{item['_id']}")
+        if "Select A Project" not in st.selectbox("Please select a Project", [selection]):
+            with st.form("projectMaterial"):
+                multiSelect = st.multiselect("Select Material Used", [material])
+                for x in multiSelect:
+                    amount = st.number_input(f"How much of {x} was used?")
+                if st.form_submit_button("Submit"):
+                    st.experimental_rerun()
+    with col2:
+        st.subheader("Add a new material!")
+        with st.form("newMaterial", clear_on_submit=True):
+            st.text_input("Enter material name: ")
+            st.number_input("Enter material cost(Please leave out the $): ", value=0)
+            st.form_submit_button("submit")
