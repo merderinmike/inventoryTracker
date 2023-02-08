24e68d3fe8f869afd59c5b8e00a67cee9706aa2b
merderinmike <alecshehorn97@gmail.com>
2023-02-07 21:13:17 -0600

    Added project materials to DB, section for adding materials to project

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import time
 from pymongo import MongoClient
 from dotenv import load_dotenv, find_dotenv
+
 # ---page setup---
 st.header("Project material tracker")
 st.subheader("Add project material data to a database and create a spreadsheet from the data")
@@ -15,6 +16,7 @@ cluster = MongoClient(
 db = cluster["materialTracker"]
 projects = db['Projects']
 materials = db['Materials']
+projectsMaterial = db['projectMaterial']
 
 # ---Create tabs, columns---
 tab1, tab2 = st.tabs(['Add Project', 'Add Project Material'])
@@ -25,40 +27,54 @@ with tab1:
     with st.form("addProject", clear_on_submit=True):
         projectName = st.text_input("Enter the project name")
         projectLocation = st.text_input("Please enter the facility for the project")
-        projectStart = st.date_input("What date did the project start?")
-        #projectEnd = st.date_input("What date did the project end?")
-        #projectLength = projectEnd - projectStart
+        projectStart = str(st.date_input("What date did the project start?"))
+        # projectEnd = st.date_input("What date did the project end?")
+        # projectLength = projectEnd - projectStart
         if st.form_submit_button("Submit"):
             if projects.count_documents({'_id': projectName}) != 0:
                 st.error("Project already exists!")
             else:
-                db['Projects'].insert_one({"_id":projectName, "Location":projectLocation,"Start":projectStart})
+                db['Projects'].insert_one(
+                    {"_id": projectName, "Location": projectLocation, "Start": projectStart, "materials": []})
                 st.success("Project created successfully!")
                 time.sleep(2)
                 st.experimental_rerun()
 
 with tab2:
     col1, col2 = st.columns(2)
+    # ---Add Material to project---
     with col1:
         st.subheader("Add material to a project")
-        #projectSelect = list(projects.find())
-        #findMaterials = list(materials.find())
+        projectSelect = list(projects.find())
+        findMaterials = list(materials.find())
         selection = ["Select A Project"]
         material = []
-        #for project in projectSelect:
-            #selection.append(f"{project['_id']}")
-        #for item in findMaterials:
-            #material.append(f"{item['_id']}")
-        if "Select A Project" not in st.selectbox("Please select a Project", [selection]):
-            with st.form("projectMaterial"):
-                multiSelect = st.multiselect("Select Material Used", [material])
-                for x in multiSelect:
-                    amount = st.number_input(f"How much of {x} was used?")
-                if st.form_submit_button("Submit"):
-                    st.experimental_rerun()
+        data = []
+        for project in projectSelect:
+            selection.append(f"{project['_id']}")
+        for item in findMaterials:
+            material.append(f"{item['_id']}")
+        projectSelection = st.selectbox("Please select a Project", selection)
+        if "Select A Project" not in projectSelection:
+            amount = {}
+            multiSelect = st.multiselect("Select Material Used", material)
+            area = st.text_input("Please enter where this material was used in the project")
+            for x in multiSelect:
+                amount[x] = st.number_input(f"How much of {x} was used?", value=0)
+            if st.button("Submit"):
+                projectsMaterial.insert_one({"_id": area, "Projects_id": projectSelection, "materialUsed": amount})
+                st.experimental_rerun()
+    # ---Add Material to DB---
     with col2:
         st.subheader("Add a new material!")
         with st.form("newMaterial", clear_on_submit=True):
-            st.text_input("Enter material name: ")
-            st.number_input("Enter material cost(Please leave out the $): ", value=0)
-            st.form_submit_button("submit")
+            materialName = st.text_input("Enter material name: ")
+            materialCost = st.number_input("Enter material cost(Please leave out the $): ", value=0)
+            if st.form_submit_button("submit"):
+                if materials.count_documents({'_id': materialName}) != 0:
+                    st.error("Material already exists!")
+                else:
+                    materials.insert_one({"_id": materialName, "cost": materialCost})
+                    st.success("Material added successfully!")
+                    time.sleep(2)
+                    st.experimental_rerun()
